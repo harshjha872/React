9e57c287a704eb60d60efb3cf604436c3e8361c5
harshjha872 <harsh.1923ec1128@kiet.edu>
2021-06-15 23:26:35 +0530

    styled-components

--- first/src/App.js
+++ first/src/App.js
@@ -2,8 +2,17 @@ import React, { useState } from 'react';
 import './style.css';
 import Form from './Form';
 import DateBox from './date';
-
+import styled from 'styled-components';
 const App = () => {
+  const Button = styled.button`
+    background: transparent;
+    border-radius: 3px;
+    border: 2px solid palevioletred;
+    color: palevioletred;
+    margin: 0 1em;
+    padding: 0.25em 1em;
+  `;
+
   const [TopicContents, setTopicContent] = useState([
     {
       Topic: 'Java',
@@ -29,12 +38,19 @@ const App = () => {
     ]);
   };
 
+  let conTent = <p>No subject studied.</p>;
+
+  if (TopicContents.length > 0) {
+    conTent = TopicContents.map((curr) => (
+      <DateBox Topic={curr.Topic} time={curr.time} date={curr.date} />
+    ));
+  }
+
   return (
     <div>
       <Form displayValues={displayValueFunction} />
-      {TopicContents.map((curr) => (
-        <DateBox Topic={curr.Topic} time={curr.time} date={curr.date} />
-      ))}
+      {conTent}
+      <Button />
     </div>
   );
 };
